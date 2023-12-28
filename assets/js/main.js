87677e6b7a613e8ffa1779a2b5c2cccfaf89627f
renehellmich <renehellmich94@gmail.com>
2023-12-28 11:25:33 +0100

    push for farid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 import {movies} from './DB.js';
 
 const loadVariables = () => {
-
     const searchString = document.getElementById("searchstring");
     const buttonSearch = document.getElementById("button_search");
     const buttonYearUp = document.getElementById("button_yearUp");
@@ -9,6 +8,9 @@ const loadVariables = () => {
     const buttonBestRate = document.getElementById("button_bestRate");
 
     const movieSection = document.getElementsByClassName("section__movies");
+
+
+    /*
     const movieContainer = document.getElementsByClassName("div__movieContainer");
     const movieHead = document.getElementsByClassName("movieHead");
     const movieYear = document.getElementsByClassName("movieYear");
@@ -16,21 +18,42 @@ const loadVariables = () => {
     const movieLength = document.getElementsByClassName("movieLength");
     const movieGenre = document.getElementsByClassName("movieGenre");
     const movieRate = document.getElementsByClassName("movieRate");
+    */
+
+    return object
 
 }
 
-const getDivMovie = (object, v) => {
+const getDivMovie = (object) => {
 
+    const [head, year, author, length, genre, rate] = object
+
+    const movie = 
+    `
+    <div class="div__movieContainer">
+    <h2 class="movieHead">${head}</h2>
+    <p class="movieYear">${year}</p>
+    <h3 class="movieAuthor">${author}</h3>
+    <p class="movieLength">${length}</p>
+    <p class="movieGenre">${genre}</p>
+    <p class="movieRate">${rate}</p>
+    </div>
+    `
+
+    console.log(movie);
+    return movie
 }
 
 const loadMovies = (v) => {
 
-    console.log(v.buttonSearch);
+    console.log(this.movieSection.innerHTML);
 
     movies.forEach((movie) => {
-        v.movieSection.innerHTML += getDivMovie(movie, v)
+        console.log(movie);
+        v.movieSection.innerHTML += getDivMovie(movie)
     });
 
 }
 
+loadVariables();
 loadMovies(loadVariables)
